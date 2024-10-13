27b6bf75ed89c5d73e5ed0d41865678a2cdd544b
R-Ram95 <rohinesh@ualberta.ca>
2024-10-13 11:02:51 -0600

    simplify stack and add oac

--- frontend/infrastructure/bin/frontend.ts
+++ frontend/infrastructure/bin/frontend.ts
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { StorageStack } from "../lib/storage-stack";
 import * as dotenv from "dotenv";
+import { WebAppStack } from "../lib/web-app-stack";
 dotenv.config();
 
 const APP_NAME = process.env.APP_NAME!;
+const ROOT_DOMAIN = process.env.ROOT_DOMAIN!;
 
 const app = new cdk.App();
 
-const assetStorage = new StorageStack(app, `${APP_NAME}-FEAssetStorageStack`, {
+new WebAppStack(app, `${APP_NAME}-WebStack`, {
   appName: APP_NAME,
+  rootDomain: ROOT_DOMAIN,
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
 });
+
+app.synth();
---
+++ frontend/infrastructure/lib/web-app-stack.ts
@@ -0,0 +1,74 @@
+import * as cdk from "aws-cdk-lib";
+import { RemovalPolicy, StackProps } from "aws-cdk-lib";
+import {
+  Certificate,
+  CertificateValidation,
+} from "aws-cdk-lib/aws-certificatemanager";
+import { Distribution, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
+import { S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { HostedZone } from "aws-cdk-lib/aws-route53";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketAccessControl,
+  ObjectOwnership,
+} from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
+
+interface WebAppStackProps extends StackProps {
+  appName: string;
+  rootDomain: string;
+}
+
+export class WebAppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: WebAppStackProps) {
+    super(scope, id, props);
+
+    const { appName, rootDomain } = props ?? {};
+
+    if (!appName || !rootDomain) return;
+
+    /** STORAGE */
+    const assetBucket = new Bucket(this, `${appName}-WebAppBucket`, {
+      versioned: true,
+      bucketName: `${props?.appName.toLowerCase()}-web-app-bucket-${
+        this.account
+      }-${this.region}`,
+      accessControl: BucketAccessControl.PRIVATE,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      objectOwnership: ObjectOwnership.BUCKET_OWNER_ENFORCED,
+    });
+
+    /** DNS */
+    const hostedZone = HostedZone.fromLookup(this, `HostedZone`, {
+      domainName: rootDomain ?? "",
+    });
+
+    const certificate = new Certificate(this, `${appName}-WebAppCertificate`, {
+      domainName: `${appName.toLowerCase()}.${rootDomain}`,
+      subjectAlternativeNames: [`www.${appName.toLowerCase()}.${rootDomain}`],
+      validation: CertificateValidation.fromDns(hostedZone),
+    });
+
+    /** CDN */
+    new Distribution(this, `${appName}-WebAppDistribution`, {
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessControl(assetBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: "index.html",
+      certificate: certificate,
+      domainNames: [
+        `${appName.toLowerCase()}.${rootDomain}`,
+        `www.${appName.toLowerCase()}.${rootDomain}`,
+      ],
+    });
+
+    new BucketDeployment(this, "WebAppBucketDeployment", {
+      destinationBucket: assetBucket,
+      sources: [Source.asset("../dist")],
+    });
+  }
+}
